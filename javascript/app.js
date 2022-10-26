02520178f3e9f3b52d6db8bf3692f1f1e9634f17
Aurelien49 <92448486+AurelienV49@users.noreply.github.com>
2022-10-26 09:13:46 +0200

    Ajout fichier app modifié

--- javascript/app.js
+++ javascript/app.js
@@ -1,167 +1,10 @@
-let mesLieuxJson = [{
-        'name': 'Ynov',
-        'lat': '47.205412',
-        'long': '-1.539511',
-        'zoom': 14,
-        "markers": [{
-                'name': 'p1',
-                'lat': '47.195412',
-                'long': '-1.519511',
-                'id': 'DeviceID_djc*ds--c--s-xsxvj',
-                'name': 'dzad',
-                'date': '2022-10-01'
-            },
-            {
-                'name': 'p2',
-                'lat': '47.205412',
-                'long': '-1.529511',
-                'id': 'DeviceID_djcxx26626c+ lknjknk j',
-                'name': 'dzad',
-                'date': '2022-10-02'
-            },
-            {
-                'name': 'p3',
-                'lat': '47.215412',
-                'long': '-1.529511',
-                'id': 'DeviceID_djcgsj6265d d dd55d5d5dvj',
-                'name': 'dzad',
-                'date': '2022-10-03'
-            },
-            {
-                'name': 'p4',
-                'lat': '47.205412',
-                'long': '-1.539511',
-                'id': 'DeviceID_djcgsjg++ -- + -- +- ++-- ',
-                'name': 'dzad',
-                'date': '2022-10-03'
-            },
-        ]
-    },
-    {
-        'name': 'Barcelone',
-        'lat': '41.37516155395602',
-        'long': '2.1805328686532555',
-        'zoom': 14,
-        "markers": [{
-                'name': 'p1',
-                'lat': '41.370131922916364',
-                'long': '2.170917925465945',
-                'id': 'DeviceID_djcgsjg*j+++++++',
-                'name': 'dzad',
-                'date': '2022-10-05'
-            },
-            {
-                'name': 'p2',
-                'lat': '41.38072319796469',
-                'long': '2.185066437150427',
-                'id': 'DeviceID_djcgsjg*j--------',
-                'name': 'dzad',
-                'date': '2022-10-06'
-            },
-            {
-                'name': 'p3',
-                'lat': '41.38842171984522',
-                'long': '2.18667207599319',
-                'id': 'DeviceID_djcgsjg*jhsdvjhv6566666  sbvj',
-                'name': 'dzad',
-                'date': '2022-10-07'
-            },
-            {
-                'name': 'p4',
-                'lat': '41.368651399046996',
-                'long': '2.188202155361436',
-                'id': 'DeviceID_djcgsjg*jdd bvj',
-                'name': 'dzad',
-                'date': '2022-10-08'
-            },
-        ]
-    },
-    {
-        'name': 'Tunisie',
-        'lat': '36.79605994054178',
-        'long': '10.189515685441393',
-        'zoom': 10,
-        "markers": [{
-                'name': 'p1',
-                'lat': '36.809693891900885',
-                'long': '10.263344487075914',
-                'id': 'DeviceID_djcgsj6559 + kbhj vv 55 2vj',
-                'name': 'dzad',
-                'date': '2022-10-09'
-            },
-            {
-                'name': 'p2',
-                'lat': '36.82546602613215',
-                'long': '10.19768274817733',
-                'id': 'DeviceID_djcgsjg* jhgjh 662 cdc',
-                'name': 'dzad',
-                'date': '2022-10-10'
-            },
-            {
-                'name': 'p3',
-                'lat': '36.76073493215711',
-                'long': '10.17618096796577',
-                'id': 'DeviceID_djcgsjg*jhsdvj ',
-                'name': 'dzad',
-                'date': '2022-10-11'
-            },
-            {
-                'name': 'p4',
-                'lat': '36.74907213447651',
-                'long': '10.224569007803549',
-                'id': 'DeviceID_djcgsjg*jhsdv  4554545 jhvsdbjhdsbvj',
-                'name': 'dzad',
-                'date': '2022-10-12'
-            },
-        ]
-    },
-    {
-        'name': 'San Miguel De Allende',
-        'lat': '20.915074607995553',
-        'long': '-100.74343635377195',
-        'zoom': 10,
-        "markers": [{
-                'name': 'p1',
-                'lat': '20.708187032182256',
-                'long': '-100.45715637810798',
-                'id': 'DeviceID_465----cdcddccc',
-                'name': 'dzad',
-                'date': '2022-10-13'
-            },
-            {
-                'name': 'p2',
-                'lat': '20.589266712960114',
-                'long': '-100.3999003829752',
-                'id': 'DeviceID_151451',
-                'name': 'dzad',
-                'date': '2022-10-14'
-            },
-            {
-                'name': 'p3',
-                'lat': '21.0000382679492',
-                'long': '-100.38723312741493',
-                'id': 'DeviceID_djcgsjg*jhsdvjhvsdbjhdsbvj',
-                'name': 'dzad',
-                'date': '2022-10-15'
-            },
-            {
-                'name': 'p4',
-                'lat': '21.16134478336257',
-                'long': '-100.92787159473079',
-                'id': 'DeviceID_mkdscmkcd   sùcd',
-                'name': 'dzad',
-                'date': '2022-10-16'
-            },
-        ]
-    }
-];
-
 // Différents lieux
-let $map = document.querySelector('#map');
+let $map = document.querySelector("#map");
 
-
+const urlUsers = 'http://localhost:3000/users/'
 var tableauMarqueurs = [];
-
+var listeUsers = [];
+var listeNomVilleUsers = [];
 var lieuxActif;
 
 class LeafletMap {
@@ -170,17 +13,34 @@ class LeafletMap {
         this.map = $map;
     }
 
+    /* Getters & setters */
+    getMap() { return this.map; }
+
+    suppressionMap() {
+        this.map.off();
+        this.map.remove();
+    }
+
+    creerNouvelleMap() {
+        this.map.createPane();
+    }
+
+    allerA(lieuxActif) {
+        this.map.flyTo([lieuxActif['lat'], lieuxActif['long']], lieuxActif['zoom']);
+        this.addMarker(lieuxActif["markers"]);
+    }
+
 
     load(e, lieuRandom) {
 
-        this.map = L.map("map").setView([lieuRandom['lat'], lieuRandom['long']], lieuRandom['zoom']).on('click', this.markerOnClick);
+        this.map = L.map("map").setView([lieuRandom["lat"], lieuRandom["long"]], lieuRandom["zoom"]).on("click", this.markerOnClick);
 
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
             maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a>"
         }).addTo(this.map);
 
-        this.addMarker(lieuRandom['markers']);
+        this.addMarker(lieuRandom["markers"]);
     }
 
 
@@ -194,7 +54,7 @@ class LeafletMap {
 
         // var newMarker = new L.marker(e.latlng).addTo(this.map);
 
-        var marqueur = new L.Marker([parseFloat(e.latlng['lat']), parseFloat(e.latlng['lng'])], { icon: icone }).addTo(this.map);
+        var marqueur = new L.Marker([parseFloat(e.latlng["lat"]), parseFloat(e.latlng["lng"])], { icon: icone }).addTo(this.map);
     }
 
     addMarker(markers) {
@@ -207,22 +67,22 @@ class LeafletMap {
         })
 
         markers.forEach(e => {
-            var marqueur = L.marker([parseFloat(e['lat']), parseFloat(e['long'])], { icon: icone }).addTo(this.map);
+            var marqueur = L.marker([parseFloat(e["lat"]), parseFloat(e["long"])], { icon: icone }).addTo(this.map);
 
-            var lat = parseFloat(e['lat']) + 0.002;
-            var long = parseFloat(e['long']);
+            var lat = parseFloat(e["lat"]) + 0.002;
+            var long = parseFloat(e["long"]);
 
             var popup = L.popup()
                 .setLatLng([lat, long])
-                .setContent('<p > -- -- -- -- -- -- -- - Details device-- -- -- -- -- -- -- - </p>' +
-                    '<p>ID : ' + e['id'] + '</p>' +
-                    '<p>Name : ' + e['name'] + '</p>' +
-                    '<p>Date : ' + e['date'] + '</p>' +
-                    '<hr>' +
-                    '<button type="button" class="btn btn-primary mb-2 btn-sm">Settings</button>' + ' ' +
-                    '<button type="button" class="btn btn-secondary mb-2 btn-sm">Mobility</button>' + ' ' +
-                    '<button type="button" class="btn btn-success mb-2 btn-sm">Graph</button>' + ' ' +
-                    '<button type="button" class="btn btn-danger mb-2 btn-sm">Delete</button>'
+                .setContent("<p > -- -- -- -- -- -- -- - Details device-- -- -- -- -- -- -- - </p>" +
+                    "<p>ID : " + e["id"] + "</p>" +
+                    "<p>Name : " + e["name"] + "</p>" +
+                    "<p>Date : " + e["date"] + "</p>" +
+                    "<hr>" +
+                    "<button type='button' class='btn btn-primary mb-2 btn-sm'>Settings</button>" + " " +
+                    "<button type='button' class='btn btn-secondary mb-2 btn-sm'>Mobility</button>" + " " +
+                    "<button type='button' class='btn btn-success mb-2 btn-sm'>Graph</button>" + " " +
+                    "<button type='button' class='btn btn-danger mb-2 btn-sm'>Delete</button>"
 
                 )
                 .openOn(this.map);
@@ -235,17 +95,102 @@ class LeafletMap {
 
 let map = new LeafletMap();
 
-let initMap = async function() {
-
-    const mesLieux = Object.values(mesLieuxJson)
-
-    // Sélectionne un lieu random du fichier JSON
-    var a = parseInt(Math.random() * Object.keys(mesLieux).length);
-
-    const lieuRandom = mesLieux[a]
-
-    // Mémorise le lieu actif
-    lieuxActif = lieuRandom;
-
+async function initMap(lieuRandom) {
     map.load($map, lieuRandom);
+}
+
+
+/** Récupère toutes les données de la base MongoDB */
+const getUsersData = () => {
+
+    const endpoint = urlUsers;
+
+    fetch(endpoint, {
+            method: 'GET',
+            headers: []
+        })
+        .then(function(response) {
+            response.json().then(function(json) {
+                listeUsers = json;
+
+                /* Chargement la liste Select avec le pseudo des users */
+                // Récupère le SELECT
+                var elt = document.querySelector('#Ma_Liste');
+
+                // Vide la liste s'il elle n'est pas vide
+                while (elt.options.length > 1) {
+                    elt.remove(0);
+                }
+
+                // Remplit le SELECT avec les noms des users
+                for (i = 0; i < listeUsers.length; i++) {
+                    var option4 = new Option(listeUsers[i]['pseudo'], parseInt(i + 1));
+                    elt.options[elt.options.length] = option4;
+                }
+
+
+                const mesLieux = Object.values(listeUsers);
+
+                // Sélectionne un lieu random du fichier JSON
+                var a = parseInt(Math.random() * Object.keys(mesLieux).length);
+
+                const lieuRandom = mesLieux[a]
+
+                // Mémorise le lieu actif
+                lieuxActif = lieuRandom;
+
+                // Récupère le pseudo et change l'index de la liste user
+                for (i = 0; i < listeUsers.length; i++) {
+                    if (listeUsers[i]['pseudo'] == lieuRandom['pseudo']) {
+                        document.FormUsers.MaListe.selectedIndex = i + 1;
+                        break;
+                    }
+                }
+
+                // Met à jour les champs textes
+                document.getElementById('pseudo_2').innerHTML = lieuRandom['pseudo'];
+                document.getElementById('welcome_2').innerHTML = lieuRandom['hello'];
+
+                document.getElementById('id_avatar').src = lieuRandom.avatar;
+
+                // Init de la map avec les données
+                initMap(lieuRandom);
+            });
+        })
+        //.then(data => window.location.reload())
+        .catch(err => console.log(err));
+}
+
+/** Récupère le choix de l'utilisateur pour le user actif */
+function ChoixUser() {
+    i = document.FormUsers.MaListe.selectedIndex;
+    if (i == 0) return;
+    var ret_label = document.FormUsers.MaListe.options[i].label;
+    var ret_value = document.FormUsers.MaListe.options[i].value;
+    var ret_selected = document.FormUsers.MaListe.options[i].selected;
+
+
+
+    /*
+        if (map.getMap() != undefined) {
+            map.suppressionMap();
+        }*/
+
+
+    //recherche du user en fonction du SELECT
+    for (i = 0; i < listeUsers.length; i++) {
+        if (listeUsers[i]['pseudo'] == ret_label) {
+            // Init de la map avec les données
+            lieuxActif = listeUsers[i];
+            break;
+        }
+    }
+
+    // Met à jour les champs textes
+    document.getElementById('pseudo_2').innerHTML = lieuxActif['pseudo'];
+    document.getElementById('welcome_2').innerHTML = lieuxActif['hello'];
+
+    document.getElementById('id_avatar').src = lieuxActif.avatar;
+
+    map.allerA(lieuxActif);
 }
